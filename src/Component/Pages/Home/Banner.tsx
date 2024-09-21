96c3a2ddf4254fd7a6828c2ca5d018b19c89d1c9
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-09-21 12:15:05 +0600

    error resolved

--- src/Component/Pages/Home/Banner.tsx
+++ src/Component/Pages/Home/Banner.tsx
@@ -1,51 +1,31 @@
+import { bannerImageOne } from "@/assets";
+import Button from "@/Component/Header/ui/Button";
+import Container from "@/Container/Container";
+import Image from "next/image";
+import React from "react";
 
-import { bannerImageOne } from '@/assets';
-import Button from '@/Component/Header/ui/Button';
-import Container from '@/Container/Container';
-import Image from 'next/image';
-import React from 'react'
-
-
-// const getData = async () => {
-//   const response = await fetch(`http://localhost:3000/api/product`, {
-
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     }
-//   });
-  
-//   return response.json()
-// };
-
-export const Banner = async() => {
-
-  // const {bannerapi} = await getData()
-
-
-  
-
-
-
+export const Banner = () => {
   return (
     <div className="bg-bannerBg h-[480px] items-center">
       {/* left-side */}
       <Container className="flex justify-between items-center">
-        <div className='mt-20 space-y-5'>
+        <div className="mt-20 space-y-5">
           <p className="text-white font-semibold">Starting at $999.90</p>
           <h1 className="text-white font-bold text-6xl">
-          The best tablet Collection 2024
+            The best tablet Collection 2024
           </h1>
           <h1 className="text-white font-semibold">
-          off this week <span className="text-[#FFD441]">-40%</span> 
+            off this week <span className="text-[#FFD441]">-40%</span>
           </h1>
-          <Button className='bg-white text-black w-32  hover:text-white hover:bg-transparent hover:bg-bannerColor border hover:border-white/40 duration-300 rounded-md '>Shop now</Button>
+          <Button className="bg-white text-black w-32  hover:text-white hover:bg-transparent hover:bg-bannerColor border hover:border-white/40 duration-300 rounded-md ">
+            Shop now
+          </Button>
         </div>
-  
-        <div className='mt-20'>
+
+        <div className="mt-20">
           <Image className="w-80" src={bannerImageOne} alt="bannerImage" />
         </div>
       </Container>
     </div>
   );
-}
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,15 @@ import SecondBanner from "@/Component/Pages/Home/SecondBanner";
 import { getData } from "@/helper";
 
 export default async function Home() {
+  const endPoint = `https://dummyjson.com/products`;
 
-  const endPoint =  `https://dummyjson.com/products`
-
-
-const product = await getData(endPoint)
+  const product = await getData(endPoint);
 
   return (
     <main>
       <Banner />
-      <ProductList product={product}/>
-      <SecondBanner />
+      <ProductList product={product} />
+      {/* <SecondBanner /> */}
       <h1 className="text-red-600">E-Commarce-Shopy</h1>
     </main>
   );
